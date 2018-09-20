5c9b36c86bbdc6a8ada37b3598eb517475e9fc64
hisham waleed karam <hisham.karam@cartologic.com>
2018-09-20 13:51:22 +0200

    add version handler

--- cartoview/__init__.py
+++ cartoview/__init__.py
@@ -2,3 +2,8 @@ from .celery import app
 
 __version__ = (1, 8, 2, 'rc', 1)
 __compatible_with__ = []
+
+
+def get_current_version():
+    from .version import get_version
+    return get_version(__version__)
