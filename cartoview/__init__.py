24f0d09bddedd1251efbc220e61ec02d1313b760
hisham waleed karam <hisham.karam@cartologic.com>
2018-09-20 13:14:03 +0200

    add layer_json view

--- cartoview/__init__.py
+++ cartoview/__init__.py
@@ -1,6 +1,2 @@
-__version__ = (1, 4, 4, 'final', 0)
-__compatible_with__ = [
-    (1, 4, 0, 'final', 0),
-    (1, 4, 1, 'final', 0),
-    (1, 4, 2, 'final', 0)
-]
+__version__ = (1, 5, 0, 'rc', 2)
+__compatible_with__ = []
